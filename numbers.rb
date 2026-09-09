d866a52794fd994e6a0bb666ddca10e856f3335c
FLemon <jinfengxie1986@gmail.com>
2026-09-09 10:47:04 +0100

    Complete number exercises

--- numbers.rb
+++ numbers.rb
@@ -4,10 +4,13 @@ num10 = 10
 string8 = '8'
 one = 1
 
+p [num10, string8, one].map(&:to_f).sum
 
 # 2. Write a loop that will print only numbers divisible by 3 between 20 and 100.
+(20..100).each { |n| p n if n % 3 == 0 }
 
 
 # 3. Find the class average given the following test scores.
 
 scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98]
+p scores.sum.to_f / scores.size
